5ded2aa2f401797139ba2d77240eaa48f9a0cef0
MMB-Laptop <mmboudot@gmail.com>
2023-09-13 18:21:50 +0200

    :test_tube: new tests to help to understand data manipulation in Sequelize

--- test.js
+++ test.js
@@ -10,7 +10,9 @@ const {
 } = require("./app/models/assoc.js");
 
 // To check Models (require the associated Model)
-const userCheck = async () => {
+const Tests = async () => {
+
+  // Create
   const newUser = await User.create({
     // id: 420, // Si non fourni : undefined = A_I, si fourni : ajout, si 0 ou egal à existant : New Error
     firstname: "Maxime",
@@ -20,18 +22,30 @@ const userCheck = async () => {
   });
   console.log(`Added user Id: ${newUser.id}`);
 
+  // FindByPk
   const getTagThree = await Tag.findByPk(3);
   console.log(getTagThree.name);
 
+  // FindAll
   const getQuestionsList = await Question.findAll({ raw: true });
   console.log(getQuestionsList);
 
+  // FindByPk + include
   const getQuestionById = await Question.findByPk(20, {
     include: ["level"],
   });
   console.log(getQuestionById);
   console.log(`QUESTION : ${getQuestionById.text}`);
   console.log(`With Level Tag : ${getQuestionById.level.name}`);
+
+  // Looping through included datas
+  const quiz = await Quiz.findByPk(1, {
+    include: ['tagList']
+  });
+  console.log(quiz);
+  console.log(`Quiz title : ${quiz.title}`);
+  console.log("Quiz tagsList" + quiz.tagsList.map(tag => tag.name));
+
 };
-userCheck();
+Tests();
 */
